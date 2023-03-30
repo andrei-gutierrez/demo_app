7ce293fbe6861acc9e2c40eff3a88c93585eb72c
andrei-gutierrez <76049998+andrei-gutierrez@users.noreply.github.com>
2023-03-30 14:38:31 +0200

    Update app.js

--- smallapp/app.js
+++ smallapp/app.js
@@ -2,13 +2,13 @@
 const apm = require('elastic-apm-node').start({
   // Override service name from package.json
   // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-  // serviceName: '',
+  serviceName: 'DemoAPM',
 
   // Use if APM Server requires a token
   // secretToken: '',
 
   // Use if APM Server uses API keys for authentication
-  // apiKey: '',
+  apiKey: 'Ulg2RE1vY0JfbDJ3alhjYnU2Qms6bVhZbEhkVEZRUC1YX0labER5N0NqZw==',
 
   // Set custom APM Server URL (default: http://127.0.0.1:8200)
   serverUrl: 'https://localhost:8200',
